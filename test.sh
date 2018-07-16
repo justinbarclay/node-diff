46dc05de3e9f5293f27141fff3fe830a386c9fbb
Justin Barclay <justincbarclay@gmail.com>
2018-07-15 18:45:29 -0600

    Simplify code by removing index transform functions

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-node --prof index.js test/test.txt test/test2.txt
-file=$(find isolate-*)
-node --prof-process $file > processed.txt
-rm $file
+node index.js test/test.txt test/test2.txt
+# node --prof index.js test/test.txt test/test2.txt
+# file=$(find isolate-*)
+# node --prof-process $file > processed.txt
+# rm $file
